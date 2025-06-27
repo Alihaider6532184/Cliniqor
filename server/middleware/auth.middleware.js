48c867d7f2a21d2047808bc3afcdceb53bd091c8
Ali Haider Awa <top043126@gmail.com>
2025-06-28 04:57:55 +0500

    fix: correct middleware export to resolve crash

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = function(req, res, next) {
+exports.protect = function(req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
 
